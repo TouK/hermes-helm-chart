d20d2f5e49326e55172d228c995533d6c3ad545d
Maciej Brzeziński <mab816813@gmail.com>
2022-02-03 10:52:38 +0100

    logs for hermes

--- test.sh
+++ test.sh
@@ -8,6 +8,10 @@ function logOnExit {
     kubectl get pod
     kubectl logs job/$RELEASE-test-job test-job || echo "Failed to log job..."
     kubectl describe job/$RELEASE-test-job || echo "Failed to describe job..."
+
+    MANAGEMENT_POD=$(kubectl get pod | grep -o -e "$RELEASE-management\-\w\+\-\w\+")
+    kubectl logs "$MANAGEMENT_POD"
+#    kubectl logs job/$RELEASE-frontend
 }
 trap 'logOnExit' EXIT
 
