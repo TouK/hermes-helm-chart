3d6ce96e0473a6c97d205b1d019ccfae5a9642ed
Maciek Próchniak <mpr@touk.pl>
2022-03-07 19:11:55 +0100

    logging...

--- test.sh
+++ test.sh
@@ -6,10 +6,13 @@ RELEASE=$1
 
 function logOnExit {
     kubectl get pod
+   
     kubectl logs job/$RELEASE-test-job test-job || echo "Failed to log job..."
     kubectl describe job/$RELEASE-test-job || echo "Failed to describe job..."
 
     MANAGEMENT_POD=$(kubectl get pod | grep -o -e "$RELEASE-management\-\w\+\-\w\+")
+    kubectl exec $MANAGEMENT_POD -- nslookup $RELEASE-kafka-0.$RELEASE-kafka-headless.default.svc.cluster.local
+
     kubectl logs "$MANAGEMENT_POD"
     kubectl logs $RELEASE-zookeeper-0
     kubectl logs $RELEASE-kafka-0
